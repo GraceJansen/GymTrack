2613377c9251b1f5d142b92da119b5611e326f79
Liam Hampton <liam.hampton@ibm.com>
2020-01-05 17:14:36 +0000

    Initial commit of the basic web app routes using ejs
    Signed-off-by: Liam Hampton <liam.hampton@ibm.com>

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000;
+
+// enable the passing of JSON
+app.use(bodyParser.urlencoded({  extended: false}));
+
+app.set('view engine', 'ejs');
+
+// home route
+app.get('/', function(req, res){
+	res.render('pages/index', {title: 'Gym Stats'}); //response
+});
+
+// used for posting data to the server
+app.post('/message', function(req, res){
+
+	const message = req.body.message;
+	res.send('message resp is: ' + message);
+});
+
+app.listen(port, () => {
+    console.log("Sever is listening on port ", port);
+})
